742851adf760af8be345cd30abf7cc24b439b619
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2019-11-11 12:42:23 +0530

    day#1 update#3

---
+++ hello_world_flutter/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//start of dart application
+void main(){
+  //should run a flutter application
+  runApp(Text(
+    'Hello World',
+    textDirection: TextDirection.ltr,));
+}
