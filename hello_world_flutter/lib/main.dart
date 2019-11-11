7c6680e404e5dbc2b1ff839fa5bc4a5fcc01185f
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2019-11-11 14:02:03 +0530

    day#1 update#4

--- hello_world_flutter/lib/main.dart
+++ hello_world_flutter/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 //start of dart application
 void main(){
   //should run a flutter application
+
   runApp(Text(
     'Hello World',
     textDirection: TextDirection.ltr,));
--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 ### Import GIST
 
 https://dartpad.dartlang.org/f0af89b42adeabe3bbe33d19f167a07e
+
+https://dartpad.dartlang.org//e55ea937c4f471c801b03aa806810a1f
